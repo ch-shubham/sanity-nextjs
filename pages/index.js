568a339ee17e4fa9b39220942bce15270acb76bb
ch-shubham <shubham128166@gmail.com>
2021-10-15 15:18:42 +0530

    Example for getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import CardListItem from "components/CardListItem";
 
 import { getAllBlogs } from "lib/api";
 
-export default function Home({ blogs }) {
+export default function Home({ blogs, randomNumber }) {
   return (
     <PageLayout>
       <Row>
@@ -15,6 +15,7 @@ export default function Home({ blogs }) {
         </Col>
       </Row>
       <hr />
+      <h1>{randomNumber}</h1>
       <Row className="mb-5">
         {/* <Col md="10">
           <CardListItem />
@@ -31,12 +32,34 @@ export default function Home({ blogs }) {
 
 // function is called during the build (build time)
 // Provides the props to your page and it creates the static pages.
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const randomNumber = Math.random();
+//   const blogs = await getAllBlogs();
+//   return {
+//     // return the prop object which is again a object havng the propNames and the values to it
+//     props: {
+//       blogs,
+//     },
+//   };
+// }
+
+export async function getServerSideProps() {
+  const randomNumber = Math.random();
   const blogs = await getAllBlogs();
   return {
     // return the prop object which is again a object havng the propNames and the values to it
     props: {
       blogs,
+      randomNumber,
     },
   };
 }
+
+// Static Page
+// Faster, can be cached by using CDN
+// Created at build time
+// When we making the request we are always receiving the sae html documemt.
+
+// Dynamic Page
+// Created at request time(we can fetch data on server)
+// Little bit slower, the time depemds on the data you are fetching
