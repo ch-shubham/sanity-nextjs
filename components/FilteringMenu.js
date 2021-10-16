57d1e6b25d506c2cce4219090a301562f7a3004a
ch-shubham <shubham128166@gmail.com>
2021-10-17 00:36:32 +0530

    Adding conditional rendering to render the button.

--- components/FilteringMenu.js
+++ components/FilteringMenu.js
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const LIST_VIEW_ICONS = ["list-ul", "border-all"];
+
 const FilteringMenu = ({ onChange, filter }) => {
   return (
     <div className="filtering-menu mb-2">
       <FontAwesomeIcon
         className="clickable hoverable"
-        size="lg"
-        icon="list-ul"
+        size="2x"
+        icon={LIST_VIEW_ICONS[filter.view.list]}
         onClick={() => onChange("view", { list: +!filter.view.list })}
       />
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { faBorderAll, faListUl } from "@fortawesome/free-solid-svg-icons";
 
+config.autoAddCss = false; // performance boost. It stops importing automatic css files as we are doing it below
 library.add(faListUl, faBorderAll);
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
