247b38a1087eb3392ba4a001d507cfb862fe814f
ch-shubham <shubham128166@gmail.com>
2021-10-16 13:18:37 +0530

    Using google fonts.

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,13 +1,25 @@
 import { Container } from "react-bootstrap";
 import Footer from "./Footer";
 import BlogNavbar from "./Navbar";
+import Head from "next/head";
 
 export default function PageLayout({ children, className }) {
   return (
-    <Container>
-      <BlogNavbar />
-      <div className={`page-wrapper ${className}`}>{children}</div>
-      <Footer />
-    </Container>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Container>
+        <BlogNavbar />
+        <div className={`page-wrapper ${className}`}>{children}</div>
+        <Footer />
+      </Container>
+    </>
   );
 }
