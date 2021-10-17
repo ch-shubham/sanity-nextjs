e7736eed71b193a71d48d8747a8d1c97a2320cf9
ch-shubham <shubham128166@gmail.com>
2021-10-17 13:38:12 +0530

    using useSWR to fetch data using nextjs api and client side fetching

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,16 @@ import CardListItem from "components/CardListItem";
 import { getAllBlogs } from "lib/api";
 import FilteringMenu from "components/FilteringMenu";
 import { useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home({ blogs }) {
   const [filter, setFilter] = useState({
     view: { list: 0 },
   });
+
+  const { data, error } = useSWR("api/hello", fetcher);
 
   return (
     <PageLayout>
