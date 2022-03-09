72ba9a1de0a227acc73ded0836a43a00e7684d59
ch-shubham <shubham128166@gmail.com>
2022-03-09 21:08:28 +0530

    adding feature to clear cookies and exit preview.

--- components/PreviewAlert.js
+++ components/PreviewAlert.js
@@ -5,7 +5,7 @@ export default function PreviewAlert() {
     <Alert variant="secondary">
       This is the preview mode!!{" "}
       {/** This will lead me to API route that will remove preview cookies */}
-      <Alert.Link href="#">Leave Preview Mode</Alert.Link>
+      <Alert.Link href="/api/exit-preview">Leave Preview Mode</Alert.Link>
     </Alert>
   );
 }
